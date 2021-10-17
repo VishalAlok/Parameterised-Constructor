a40b82701bc324dc8a0edb2eaa19cc8ab61120de
vishal.Alok <you@example.com>
2021-10-17 19:30:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ParameterizedConstructorExample
+{
+
+    class Geek
+    {
+        String name;
+        int id;
+        Geek(String name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public static void Main()
+        {
+            Geek geek1 = new Geek("GFG", 1);
+            Console.WriteLine("GeekName = " + geek1.name +
+                             " and GeekId = " + geek1.id);
+        }
+    }
+}
